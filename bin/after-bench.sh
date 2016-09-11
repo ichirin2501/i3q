3dd80849ff6ab93570fc0e2606ed60d0625f97b4
ichirin2501 <ichirin2501@hatena.ne.jp>
2016-09-11 13:54:02 +0900

    benchスクリプト修正

---
+++ bin/after-bench.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -ex
+
+if [ -e /var/log/nginx/isucon.access_log.tsv ]; then
+  alp -f /var/log/nginx/isucon.access_log.tsv --aggregates "/memo/\d+,/recent/\d+" > /tmp/alp_latest.txt
+fi
+
+if [ -e /var/lib/mysql/mysqld-slow.log ]; then
+  cp /var/lib/mysqld-slow.log /tmp/mysqld-slow_latest.log
+  chmod 644 /tmp/mysqld-slow_latest.log
+fi
+
