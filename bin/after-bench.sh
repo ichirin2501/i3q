53985f60da6970ca842c64c8e07c2e20d2c11954
ichirin2501 <ichirin2501@hatena.ne.jp>
2016-09-11 13:59:45 +0900

    bench直前にlogrotateだけする、after-benchのpathミス修正

--- bin/after-bench.sh
+++ bin/after-bench.sh
@@ -3,11 +3,11 @@
 set -ex
 
 if [ -e /var/log/nginx/isucon.access_log.tsv ]; then
-  alp -f /var/log/nginx/isucon.access_log.tsv --aggregates "/memo/\d+,/recent/\d+" > /tmp/alp_latest.txt
+  alp -f /var/log/nginx/isucon.access_log.tsv --aggregates "/memo/\d+,/recent/\d+" | tee /tmp/alp_latest.txt
 fi
 
 if [ -e /var/lib/mysql/mysqld-slow.log ]; then
-  cp /var/lib/mysqld-slow.log /tmp/mysqld-slow_latest.log
+  cp /var/lib/mysql/mysqld-slow.log /tmp/mysqld-slow_latest.log
   chmod 644 /tmp/mysqld-slow_latest.log
 fi
 
