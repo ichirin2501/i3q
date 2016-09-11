069c81bb3437d7c5d2c63b49e8ed7daba74a42e2
ichirin2501 <ichirin2501@hatena.ne.jp>
2016-09-11 14:50:45 +0900

    db名指定忘れてた

--- bin/pre-bench.sh
+++ bin/pre-bench.sh
@@ -9,5 +9,5 @@ cd ..
 # root
 
 # ALTER
-mysql -uroot -proot -e 'ALTER TABLE memos ADD INDEX `idx_is_private_created_at` (`is_private`, `created_at`)'
-mysql -uroot -proot -e 'ALTER TABLE memos ADD INDEX `idx_user_created_at` (`user`, `created_at`)'
+mysql -uroot -proot isucon -e 'ALTER TABLE memos ADD INDEX `idx_is_private_created_at` (`is_private`, `created_at`)'
+mysql -uroot -proot isucon -e 'ALTER TABLE memos ADD INDEX `idx_user_created_at` (`user`, `created_at`)'
